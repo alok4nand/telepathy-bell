5574e912f63ae92cff7b1d03a480984c458b8e67
Alok Anand <alok4nand@gmail.com>
2016-07-25 23:31:14 +0530

    Implemented createRingAccount() and value()

--- parameters.cpp
+++ parameters.cpp
@@ -1,10 +1,14 @@
 #include "parameters.hpp"
+#include <QDBusMetaType>
+#include <QMap>
+#include <iterator>
+#include <QDBusPendingCall>
+#include <QDBusPendingReply>
 
 using namespace Bell;
 
 Tp::ProtocolParameterList Parameters::getParameterList()
 {
-  qDebug() << Q_FUNC_INFO;
    Tp::ProtocolParameterList params;
 
    params << Tp::ProtocolParameter(QLatin1String("Username"), QDBusSignature(QLatin1String("s")), Tp::ConnMgrParamFlagRequired)
@@ -23,4 +27,53 @@ Parameters::Parameters(const QVariantMap& parameters): mParameters(parameters)
   mRingID = parameters[QLatin1String("RingID")].toString();
   mAccountID = parameters[QLatin1String("AccountID")].toString();
   mHostname = parameters[QLatin1String("Hostname")].toString();
+}
+
+void Parameters::createNewRingAccount()
+{
+  qDebug() << Q_FUNC_INFO;
+
+  qDBusRegisterMetaType<MapStringString>();
+  QDBusReply<MapStringString> reply = mRingDaemonInterface.ConfigurationManagerInterface()->call("getAccountTemplate","RING");
+  if(!reply.isValid())
+  {
+  qDebug() << "Error in getting Account Template";
+  // QDBusError mError = reply.error();
+  // qDebug() << mError.message();
+  // qDebug() << mError.name();
+  }
+  MapStringString mAccountDetails = reply.value();
+  mAccountDetails["Account.alias"]  = mUsername;
+  mAccountDetails["Account.displayName"] = mAccount;
+  mAccountDetails["Account.enable"] = "false";
+  mAccountDetails["Account.hasCustomUserAgent"] = "telepathy-bell";
+  mAccountDetails["Account.hostname"] = mHostname;
+  // QMap<QString,QString>::iterator iter;
+  // for(iter = mAccountDetails.begin(); iter != mAccountDetails.end(); ++iter)
+  // {
+  //       qDebug() << iter.key() << iter.value();
+  // }
+  QList<QVariant> argumentList;
+  argumentList << QVariant::fromValue(mAccountDetails);
+
+ QDBusPendingCall mPendingCallReply = mRingDaemonInterface.ConfigurationManagerInterface()->asyncCallWithArgumentList("addAccount",argumentList);
+ QDBusPendingReply<QString> mPendingReply(mPendingCallReply);
+ if(mPendingReply.isError()){
+   qDebug() << "Error in getting new Account ID ";
+ }
+ mAccountID = mPendingReply.value();
+}
+
+QVariantMap Parameters::value()
+{
+  QVariantMap modifiedParameters;
+  modifiedParameters[QLatin1String("Username")] = mUsername;
+  modifiedParameters[QLatin1String("Account")] = mAccount;
+  modifiedParameters[QLatin1String("RingID")] = mRingID;
+  modifiedParameters[QLatin1String("AccountID")] = mAccountID;
+  modifiedParameters[QLatin1String("Hostname")] = mHostname;
+  // for(QVariantMap::const_iterator iter = modifiedParameters.begin(); iter != modifiedParameters.end(); ++iter) {
+  //      qDebug() << iter.key() << iter.value();
+  //     }
+  return modifiedParameters;
 }
--- parameters.hpp
+++ parameters.hpp
@@ -3,17 +3,27 @@
 
 #include <TelepathyQt/ProtocolParameterList>
 #include <QVariant>
-// insert ring headers.
+#include <QMap>
+#include <iterator>
+#include <QList>
+
+#include "ringdaemon.hpp"
+
+typedef QMap<QString, QString> MapStringString;
+Q_DECLARE_METATYPE(MapStringString)
+
 namespace Bell{
 
 class Parameters{
 public:
 // Store the parameters from the account settings panel.
 Parameters(const QVariantMap& parameters);
+void createNewRingAccount();
 // Get the list of parameters that should appear in the account settings panel.
 static Tp::ProtocolParameterList getParameterList();
-
+QVariantMap value();
 private:
+  RingDaemon mRingDaemonInterface;
   const QVariantMap mParameters;
   QString mUsername;
   QString mAccount;
