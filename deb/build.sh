94589106f6904b809a2e9d29dac52d2cc3691f9e
Alok Anand <alok4nand@gmail.com>
2016-08-23 10:35:54 +0530

    Modified files in debian folder

--- deb/build.sh
+++ deb/build.sh
@@ -1,2 +1,8 @@
+# Script to generate and install telepathy-bell .deb pkg
+# Run this from `deb` directory
+
+# Build telepathy-bell .deb pkg
 dpkg-buildpackage -us -uc -b
-sudo dpkg -i ../telepathy-bell_1.0.0-0\~8.5_all.deb
+
+# Install telepathy-bell .deb pkg
+# sudo dpkg -i ../telepathy-bell_1.0.0-0\~8.5_all.deb
